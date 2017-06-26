5f62da30128547fa92216de94e6db214c46897c2
Tertiium <yang.guozhang@yahoo.com>
2017-06-26 16:53:21 -0300

    Commit - Aula 22 How to Swipe to Delete Messages in UITableView

--- GameOfChats/MessagesController.swift
+++ GameOfChats/MessagesController.swift
@@ -34,9 +34,43 @@ class MessagesController: UITableViewController {
         
         //observerMessages()
         
+        tableView.allowsMultipleSelectionDuringEditing = true
+        
     }
     
     // MARK: - Functions
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+        guard let uid = FIRAuth.auth()?.currentUser?.uid else {
+            return
+        }
+        
+        let message = self.messages[indexPath.row]
+        
+        if let chatPartnerId = message.chatPartnerId() {
+            FIRDatabase.database().reference().child("user-messages").child(uid).child(chatPartnerId).removeValue(completionBlock: { (error, ref) in
+                if error != nil {
+                    print("Fsiled to delete message: ", error ?? "")
+                    return
+                }
+                
+                self.messagesDictionary.removeValue(forKey: chatPartnerId )
+                self.attemptReleoadOfTable()
+                
+                // this is one way of updating the table, but its actually not that safe..
+                //self.messages.remove(at: indexPath.row)
+                //self.tableView.deleteRows(at: [indexPath], with: .automatic)
+                
+            })
+        }
+        
+        
+    }
+    
     func observerUserMessages() {
         guard let uid =  FIRAuth.auth()?.currentUser?.uid else{
             return
@@ -55,6 +89,15 @@ class MessagesController: UITableViewController {
                 self.fetchMessageWithMessageId(messageId: messageId)
                 
             }, withCancel: nil)
+            
+        }, withCancel: nil)
+        
+        ref.observe(.childRemoved, with: { (snapshot) in
+            print(snapshot)
+            print(self.messagesDictionary)
+            
+            self.messagesDictionary.removeValue(forKey: snapshot.key)
+            self.attemptReleoadOfTable()
             
         }, withCancel: nil)
     }
